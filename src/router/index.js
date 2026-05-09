4af13d2a16f4720d1fbf56e1e3e44e507ed0d53a
neownewo <66373192+neownewo@users.noreply.github.com>
2026-05-10 01:20:48 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Guides from '../pages/Guides.vue'
 import GuidePost from '../pages/GuidePost.vue'
+import Gacha from '../pages/GachaCalendar.vue'
+import Merch from '../pages/Guides.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/guides', component: Guides },
-  { path: '/guides/:slug', component: GuidePost },
+  {path: '/guide-post',component: GuidePost},
+  { path: '/gacha', component: Gacha },
+  { path: '/merch', component: Merch },
+
 ]
 
 export default createRouter({
