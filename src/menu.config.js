b7bb7ae48700b5a8baa33f3c005647506ba3d326
neownewo <66373192+neownewo@users.noreply.github.com>
2026-05-10 01:22:21 +0800

    Add files via upload

--- src/menu.config.js
+++ src/menu.config.js
@@ -1,7 +1,7 @@
 export const mainMenuItems = [
-  { label: '首頁主視覺', href: '/#hero', icon: '✦' },
+  { label: '首頁', href: '/#hero', icon: '✦' },
   { label: '最新消息', href: '/#news', icon: '📌' },
   { label: '遊戲攻略', href: '/#beginner', icon: '🔰' },
-  { label: '卡池歷程', href: '/#gacha', icon: '🗓️' },
+  { label: '卡池行事曆', href: '/#gacha', icon: '🗓️' },
   { label: '周邊整理', href: '/#merch', icon: '🎁' },
 ]
