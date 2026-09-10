da355df39ca63d140cf91da560e4fae2553e47cb
neownewo <66373192+neownewo@users.noreply.github.com>
2026-09-10 16:12:35 +0800

    Delete api directory

--- api/neiwneiw/logout.js
+++
@@ -1,8 +0,0 @@
-import { clearSessionCookie } from '../_lib/auth.js'
-import { onlyMethods } from '../_lib/http.js'
-
-export default function handler(req, res) {
-  if (!onlyMethods(req, res, ['POST'])) return
-  res.setHeader('Set-Cookie', clearSessionCookie())
-  res.status(200).json({ ok: true })
-}
--- api/neiwneiw/session.js
+++
@@ -1,7 +0,0 @@
-import { isAuthenticated } from '../_lib/auth.js'
-import { onlyMethods } from '../_lib/http.js'
-
-export default function handler(req, res) {
-  if (!onlyMethods(req, res, ['GET'])) return
-  res.status(200).json({ authenticated: isAuthenticated(req) })
-}
