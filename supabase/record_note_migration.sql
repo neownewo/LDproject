190d6723fb603f886163c7458060a548050409da
neownewo <66373192+neownewo@users.noreply.github.com>
2026-09-21 22:43:57 +0800

    拿掉資料庫設定

--- supabase/record_note_migration.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE public.gacha_records
-ADD COLUMN IF NOT EXISTS note VARCHAR(100);
-
-COMMENT ON COLUMN public.gacha_records.note
-IS '使用者自訂抽卡紀錄備註，最多 100 字';
-
-GRANT SELECT, INSERT, UPDATE, DELETE
-ON TABLE public.gacha_records
-TO service_role;
